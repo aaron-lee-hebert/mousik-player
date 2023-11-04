ddee5a4b4a25b3fc4d0fd8815bc40579e15f7af9
Aaron Hebert <aaron.lee.hebert@gmail.com>
2023-11-04 08:28:57 -0500

    Setup Project Files and Project References
    Create a new Visual Studio solution that contains the following
    projects:
    
    - MousikPlayer
    - MousikPlayerTests
    
    These two projects contain the boiler plate code and framework for
    future development in the repository.

---
+++ MousikPlayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MousikPlayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
